f79027190b7f694143c4d32f30c83ae2721625a7
Adam Nordquist <apnordquist@gmail.com>
2024-03-29 14:12:21 -0600

    Initialization

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace OOP_Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create instance of class
+            RecursiveMath recursiveMath = new RecursiveMath();
+            //create variables
+            int n = 0;
+            int m = 0;
+            int l = 0;
+            //first problem
+            Console.WriteLine("Addition");
+            while (n == 0)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the first number: ");
+                    n = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please Enter a valid number!");
+                }
+            }
+            while (m == 0)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter the second number: ");
+                    m = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please Enter a valid number!");
+                }
+            }
+            Console.WriteLine("Result: " + recursiveMath.AdditionBetween(n,m));
+            Console.WriteLine("Division");
+            while (l == 0)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number to divide: ");
+                    l = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Please Enter a valid number!");
+                }
+            }
+            Console.WriteLine("Result: " + recursiveMath.EvenDivision(l));
+        }
+    }
+}
---
+++ RecursiveMath.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab10
+{
+    internal class RecursiveMath
+    {
+        //attributes
+        //constructor
+        //methods
+        public int AdditionBetween(int num1, int num2) 
+        {
+            if (num1 < num2)
+            {
+                int result = 0; //create variable for result
+                for (int i = num1; i <= num2; i++) //start at num1, end at num2
+                {
+                    result += i; //add each interation of for loop to the result
+                }
+                return result; //return the sum of all numbers between num1 and num2 (inclusive)
+            }
+            else 
+            {
+                int result = 0; //create variable for result
+                for (int i = num2; i <= num1; i++) //start at num2, end at num1
+                {
+                    result += i; //add each interation of for loop to the result
+                }
+                return result; //return the sum of all numbers between num2 and num1 (inclusive)
+            }
+        }
+        public int EvenDivision(int num) 
+        { 
+            int counter = 0; //create variable to count iterations
+            while (num % 2 == 0) //check to see if even
+            {
+                num /= 2;
+                counter++;
+            }
+            return counter; //return the number of times it can be evenly divided
+        }
+    }
+}
